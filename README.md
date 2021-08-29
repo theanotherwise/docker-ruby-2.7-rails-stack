b6f3bb324943cea0bd15a1aa17ad0e12e104e5a2
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2021-08-30 01:09:09 +0200

    no message

---
+++ README.md
@@ -0,0 +1,3 @@
+# Docker Ruby on Rails Rails Stack
+
+## Repository moved to Seems Cloud organization ([link](https://github.com/seemscloud/docker-rails-on-rails-stack))
--- redis/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sed "s/^port 6379$/port ${LISTEN_PORT}/g" -i "${APPDIR}"/etc/redis.conf
-sed "s|^dir .\/$|dir /${APPDIR}\/var|g" -i "${APPDIR}"/etc/redis.conf
-sed "s/^bind 127.0.0.1 -::1$/bind 0.0.0.0/g" -i "${APPDIR}"/etc/redis.conf
-sed "s/^dbfilename dump.rdb$/dbfilename ${DB_FILENAME}/g" -i "${APPDIR}"/etc/redis.conf
-
-redis-server "${APPDIR}"/etc/redis.conf
